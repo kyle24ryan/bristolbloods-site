51d221ebf629268d875ac76a79a941d740a4e10b
kyle24ryan <45831681+kyle24ryan@users.noreply.github.com>
2025-09-02 18:28:53 -0700

    Update main.tsx

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,22 +1,6 @@
 import './index.css';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import AdminDashboard from './components/AdminDashboard.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<div className="p-4 text-white">Hello Bristol Bloods</div>} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
-    </BrowserRouter>
-  )
-}
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
